6bf229ffab3bdf0d88f816e4e43b6b9cc8a648b5
Felipe Roberto <56833987+fekzim@users.noreply.github.com>
2022-09-23 12:42:08 -0300

    First Commit
    Added the mysql.connector lib and PysimpleGui
    Created two windows
    Created the Login verifier

---
+++ CRUD.py
@@ -0,0 +1,86 @@
+import mysql.connector 
+import PySimpleGUI as sg
+
+conexao = mysql.connector.connect(
+    host='localhost',
+    user='root',
+    password='fek123',
+    database='bdtestes'
+)
+cursor = conexao.cursor()
+
+
+def janelaLogin():
+    sg.theme('Reddit')
+    layout = [
+    [sg.Text('Usuario')],
+    [sg.Input(key='usuario')],
+    [sg.Text('Senha')],
+    [sg.Input(key='senha')],
+    [sg.Button('Logar')],
+    [sg.Text('Você ainda não tentou se conectar', key='mensagem')],
+    [sg.Button('Abrir'), sg.Button('testar')]
+]
+    return sg.Window('Login', layout=layout, finalize=True)
+
+def janelaInfo():
+    sg.theme('Dark')
+    layout2 = [
+    [sg.Input('ID'), sg.Text('ABA DE PESQUISA')],
+    [sg.Text('X')],
+    [sg.Text('X')],
+    [sg.Button('Pesquisar')],
+]
+    return sg.Window('Info', layout= layout2, finalize=True)
+
+#Janela inicial
+janela1,janela2 = janelaLogin(), None
+#LOOP DE EVENTOS
+while True:
+    window, event,values = sg.read_all_windows()
+
+    #window closed
+    if window == janela1 and event == sg.WINDOW_CLOSED:
+        break
+    if window == janela2 and event == sg.WINDOW_CLOSED:
+        janela2.close()
+
+    if window == janela1 and event == 'Abrir':
+        janela2 = janelaInfo()
+    
+    if window == janela1 and event== 'Logar':
+        user = values['usuario']
+       
+        
+
+        comando = f'SELECT Login From usuarios WHERE Login = "{user}"'
+        cursor.execute(comando)
+        usuarioExiste = cursor.fetchall()
+        print(usuarioExiste)
+        if usuarioExiste != [] :
+            print("USUARIO EXISTE")
+
+            comando = f'SELECT Senha from usuarios Where Login = "{user}"'
+            cursor.execute(comando)
+            senhaBancoDeDados = cursor.fetchall()
+            senhaBancoDeDados = senhaBancoDeDados
+            senhaDigitada = values['senha']
+            s = senhaBancoDeDados[0]
+            senhaString = ''.join(s)
+            if senhaString == senhaDigitada:
+                janela2 = janelaInfo()
+            else:
+                print("ERRADO")
+        else:
+            print("USUARIO não EXISTE")
+        
+
+            
+    if window == janela1 and event== 'testar':
+        comando = 'SELECT Login FROM usuarios'
+        cursor.execute(comando)
+        valores = cursor.fetchall()
+        print(valores)
+
+cursor.close()
+conexao.close()
